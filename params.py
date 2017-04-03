097d94bf4aa58e479077e3ea8a0e2599f417c3d2
mboyanna <mboyanna@yahoo.com>
2017-04-03 08:54:45 -0700

    Update params.py
    For speedier access

--- params.py
+++ params.py
@@ -1,7 +1,7 @@
 from instagram.client import InstagramAPI
 access_token = "3463673370.9d470f7.5ef8019afecd446eaf7b74c410596676"
 client_secret = "d9520b95d70249d49ac8c0539fdf030c"
-batch_size = 2
+batch_size = 100
 burst_size = 3
 sleep_time = 2
 with_debug_logging = True
