9781da1e571892fc8739627ed1fa155d0e7c6aa1
Bojana Parman <mboyanna@gmail.com>
2017-04-03 01:16:27 -0700

    fix pip install for simpletrends

--- simpletrends/__init__.py
+++ simpletrends/__init__.py
@@ -1,13 +1,5 @@
 from .simpletrends import SimpleTrends
 
-
-def __init__(self, *args, **kwargs):
-	# self.access_token = "3463673370.9d470f7.5ef8019afecd446eaf7b74c410596676"
-	# self.client_secret = "d9520b95d70249d49ac8c0539fdf030c"
-	# self.batch_size = 2
-	# self.burst_size = 3
-	# self.sleep_time = 10
-	# self.with_debug_logging = True
-	a=2
+str=SimpleTrends
 
 
