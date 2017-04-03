cc1b617763dc8dde485cd379d855663725d5c756
Bojana Parman <mboyanna@gmail.com>
2017-04-02 23:25:28 -0700

    parametrize settings in params.py file

--- simpletrends/simpletrends.py
+++ simpletrends/simpletrends.py
@@ -3,21 +3,12 @@ from instagram.client import InstagramAPI
 from instagram.helper import timestamp_to_datetime
 from datetime import datetime, timedelta, date, time
 import time
-
-# TBD - read in from a config file for the system
-access_token = "3463673370.9d470f7.5ef8019afecd446eaf7b74c410596676"
-client_secret = "d9520b95d70249d49ac8c0539fdf030c"
-batch_size = 2
-burst_size = 3
-sleep_time = 2
-with_debug_logging = True
-instagram_api = InstagramAPI(access_token=access_token, client_secret=client_secret)
+import params
 
 class SimpleTrends(object): 
 
 	def __init__(self, *args,**kwargs):
 		super(SimpleTrends, self).__init__(**kwargs)
-
 
 	def all_recent_posts_for_tag(self, **kwargs):
 
@@ -26,7 +17,7 @@ class SimpleTrends(object):
 
 		cutoff_date = datetime.utcnow() - timedelta(timedelta_days-1)
 		
-		if with_debug_logging:
+		if params.with_debug_logging:
 			print("Time period: ", timedelta_days," days back")
 			print("Cutoff date [UTC]:", cutoff_date)
 			print("Hashtag:", tag_name)
@@ -36,28 +27,28 @@ class SimpleTrends(object):
 		recent_posts = []
 		filtered_recent_posts = []
 
-		recent_posts, next_ = instagram_api.tag_recent_media_crawl_tag(count=batch_size, tag_name=tag_name, pagination_format='next_min_tag_id', scope='public_content')
+		recent_posts, next_ = params.instagram_api.tag_recent_media_crawl_tag(count=params.batch_size, tag_name=tag_name, pagination_format='next_min_tag_id', scope='public_content')
 		all_recent_posts.extend (filter(lambda a_post, cutoff_dt=cutoff_date : (a_post.created_time > cutoff_dt), recent_posts))
 
 		burst_iter = 0
 		while next_:
-			recent_posts,next_ = instagram_api.tag_recent_media_crawl_tag(count=batch_size, tag_name=tag_name, pagination_format='next_min_tag_id',max_tag_id=next_, scope='public_content')
+			recent_posts,next_ = params.instagram_api.tag_recent_media_crawl_tag(count=params.batch_size, tag_name=tag_name, pagination_format='next_min_tag_id',max_tag_id=next_, scope='public_content')
 			filtered_recent_posts = [a_post for a_post in recent_posts[1:] if a_post.created_time > cutoff_date]
 			for post in filtered_recent_posts:
 				all_usernames.add(post.user.username)
 
-			if with_debug_logging:
+			if params.with_debug_logging:
 				all_recent_posts.extend(filtered_recent_posts)
 
 			if (len(recent_posts)>1 and recent_posts[len(recent_posts)-1].created_time <= cutoff_date):
 				break
 
 			burst_iter=burst_iter+1
-			if burst_iter>burst_size:
+			if burst_iter>params.burst_size:
 				burst_iter=0
-				time.sleep(sleep_time)
+				time.sleep(params.sleep_time)
 
-		if with_debug_logging:
+		if params.with_debug_logging:
 			return all_usernames, all_recent_posts
 		
 		return all_usernames
@@ -65,14 +56,14 @@ class SimpleTrends(object):
 	def _call_example_2_days(self):
 		all_usernames = set()
 		all_posts = []		
-		if with_debug_logging:
+		if params.with_debug_logging:
 			all_usernames, all_posts = self.all_recent_posts_for_tag(self, days_old=2, hashtag="urbandecay")
 		else:
 			all_usernames = self.all_recent_posts_for_tag(self, days_old=2, hashtag="urbandecay")
 		
 		print("List of handles who posted #urbandecay on Instagram in the last 2 (TWO) days is: ")
 		print (all_usernames)	
-		if with_debug_logging:
+		if params.with_debug_logging:
 			print("Detailed log of posts follows.\n [* Note: log times are in UTC which is PST+7hrs now is:",datetime.utcnow()," ]")
 			for post in all_posts:
 				print (post.user.username, post.created_time, post.post_text, sep=" at ")
@@ -81,14 +72,14 @@ class SimpleTrends(object):
 	def _call_example_90_days(self):
 		all_usernames = set()
 		all_posts = []		
-		if with_debug_logging:
+		if params.with_debug_logging:
 			all_usernames, all_posts = self.all_recent_posts_for_tag(self, days_old=90, hashtag="urbandecay")
 		else:
 			all_usernames = self.all_recent_posts_for_tag(self, days_old=90, hashtag="urbandecay")
 		
 		print("List of handles who posted #urbandecay on Instagram in the the last 90 days is: ")
 		print (all_usernames)	
-		if with_debug_logging:
+		if params.with_debug_logging:	
 			print("Detailed log of posts follows.\n [* Note: log times are in UTC which is PST+7hrs now is:",datetime.utcnow()," ]")
 			for post in all_posts:
 				print (post.user.username, post.created_time, post.post_text, sep=" at ")
